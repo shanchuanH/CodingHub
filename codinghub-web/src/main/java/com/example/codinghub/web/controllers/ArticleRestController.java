3dc5c9c0cc53e3d5f725518e87db5d069436ca3f
lukahong <honghongon@gmail.com>
2024-08-16 23:45:56 +0800

    add swagger annotation for ArticleRestController

--- codinghub-web/src/main/java/com/example/codinghub/web/controllers/ArticleRestController.java
+++ codinghub-web/src/main/java/com/example/codinghub/web/controllers/ArticleRestController.java
@@ -4,12 +4,14 @@ import com.example.codinghub.common.enums.OperateTypeEnum;
 import com.example.codinghub.common.vos.CommonResponse;
 import com.example.codinghub.service.service.UserFootService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/articles/operations")
+@Tag(name = "Article Operations", description = "Operations related to articles")
 public class ArticleRestController {
 
     @Resource
@@ -23,7 +25,12 @@ public class ArticleRestController {
      * @param operateCode The type of operation to perform (like, unlike, collect, uncollect).
      * @return The updated article details.
      */
-    @Operation(summary = "Operate on Article", description = "Performs an operation like liking or collecting on a specific article by ID")
+    @Operation(summary = "Operate on Article", description = "EMPTY(0, \"\"),\n" +
+            "    READ(1, \"阅读\"),\n" +
+            "    PRAISE(2, \"点赞\"),\n" +
+            "    COLLECTION(3, \"收藏\"),\n" +
+            "    CANCEL_PRAISE(4, \"取消点赞\"),\n" +
+            "    CANCEL_COLLECTION(5, \"取消收藏\"),")
     @PostMapping("/{articleId}")
     public CommonResponse<Boolean> operateOnArticle(@PathVariable String articleId,
                                                     @RequestParam String userId,
