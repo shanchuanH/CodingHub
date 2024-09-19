af8974ba251da951e6d7d2dd2bcebc00cb4c9bd9
lukahong <honghongon@gmail.com>
2024-09-19 20:58:34 +0800

    add rabbitmq support

--- codinghub-service/src/main/java/com/example/codinghub/service/service/RabbitMQSender.java
+++ codinghub-service/src/main/java/com/example/codinghub/service/service/RabbitMQSender.java
@@ -1,17 +1,20 @@
 package com.example.codinghub.service.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 @Service
+@Slf4j
 public class RabbitMQSender {
 
     @Resource
     private RabbitTemplate rabbitTemplate;
 
     public void sendPraiseNotification(String articleId, String userId) {
+        log.info("Sending praise notification for article {} by user {}", articleId, userId);
         String message = String.format("Article %s was praised by user %s", articleId, userId);
         rabbitTemplate.convertAndSend("articleExchange", "praiseNotification", message);
     }
